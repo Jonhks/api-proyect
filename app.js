6f9ef2a97495d1b3dc12ad01792980ece48f6631
Jonhks <jonhgx@gmail.com>
2021-06-03 20:09:03 -0500

    Update/subiendo cambios de la clase

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const timestamp = Date.now();
 const hash = md5(timestamp + privada + publica);
 const boton1 = document.getElementById('boton1');
 const boton2 = document.getElementById('boton2');
+const botonInicio = document.getElementById('boton-inicio');
+const botonFinal = document.getElementById('boton-final');
+
 
 let offset = 0;
+let total = 0;
 
 // const url = `http://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${publica}&hash=${hash}`;
 // const url = `http://gateway.marvel.com/v1/public/comics?limit=20&offset=${offset}&ts=${timestamp}&apikey=${publica}&hash=${hash}`;
@@ -15,9 +19,14 @@ let offset = 0;
 const fetchData = () => {
   const url = `https://gateway.marvel.com/v1/public/comics?limit=20&offset=${offset}&ts=${timestamp}&apikey=${publica}&hash=${hash}`;
   fetch(url)
-  .then(response => response.json())
-  .then(obj => printData(obj.data.results))
-  .catch(error => console.error(error))
+    .then(response => response.json())
+    .then(obj => {
+      const total = obj.data
+      printData(obj.data.results, total)
+      checkOffset(total.total);
+    })
+    .catch(error => console.error(error))
+    return total.total
 }
 
 fetchData();
@@ -25,11 +34,33 @@ fetchData();
 boton1.addEventListener('click', () => {
   offset -= 20;
   fetchData();
+  checkOffset();
 })
+
 boton2.addEventListener('click', () => {
   offset += 20;
   fetchData();
+  checkOffset();
 })
+
+botonInicio.addEventListener('click', () => {
+  offset = 0;
+  fetchData();
+  checkOffset();
+})
+
+botonFinal.addEventListener('click', () =>{
+  offset = total.innerText - (total.innerText % 20);
+  fetchData();
+  checkOffset();
+})
+
+const checkOffset = (total) => {
+  offset <= 0 ? boton1.disabled = true : boton1.disabled = false;
+  offset <= 0 ? botonInicio.disabled = true : botonInicio.disabled = false;
+  offset == total - (total % 5) ? boton2.disabled = true : boton2.disabled = false;
+  offset == total - (total % 5) ? botonFinal.disabled = true : botonFinal.disabled = false;
+}
 
 
 const getId = id => {
@@ -37,14 +68,20 @@ const getId = id => {
   fetch(url)
     .then(resp => resp.json())
     .then(obj => printDetailComic(obj.data.results))
+    comicId = id
+    getCharacterComicId(comicId)
+    return comicId
 }
 
-
-
-
-
-
-
-
-
-
+const getCharacterComicId = (id) => {
+  let offsetComic = 0; 
+  const url = `https://gateway.marvel.com/v1/public/comics/${id}/characters?limit=5&offset=${offsetComic}&ts=${timestamp}&apikey=${publica}&hash=${hash}`
+  fetch(url)
+      .then(response => response.json())
+      .then(obj => {
+        const totalComics = obj.data.total;
+        checkOffset(totalComics)
+        console.log(totalComics, offsetComic);
+        printCharactersComic(obj.data.results, comicCharactersResults, comicCharactersInfo)})
+      .catch(err => console.error(err))
+};
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const root = document.getElementById('root');
+const cajaTotal = document.getElementById('total');
+const containerCharacterInfo = document.getElementById('container-character-info');
+const comicCharactersResults = document.getElementById('comic-characters-results');
+const comicCharactersInfo = document.getElementById('comic-characters-info');
 
-const printData = arr => {
+const printData = (arr, total) => {
   const pathNonFoundNowanted = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
   const pathNonFoundWanted =   "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_uncanny"
   let cajita = '';
   arr.forEach(comic => {
-    // console.log(comic);
     const {title, thumbnail: {extension, path}, id} = comic;
     cajita += `
     <div class="column is-one-fifth" onclick="getId(${id})">
@@ -17,7 +20,8 @@ const printData = arr => {
       </figure>
     </div>`
   });
-  root.innerHTML = cajita
+  cajaTotal.innerHTML = total.total;
+  root.innerHTML = cajita;
 }
 
 const printDetailComic = arr => {
@@ -41,15 +45,42 @@ const printDetailComic = arr => {
         <p>${writer ? writer[0]?.name : 'Sin informacion'}</p>
         <h4>Descripción:</h4>
         <p>${description}</p>
+        <button onclick="fetchData()">Regresar</button>
       </div>
     </div> `
   })
   root.innerHTML = cajita
-
 }
 
 
-//  nombre ? 'hola'
+
+const printCharactersComic = (arr, containerText, container) => {
+  if(arr.length === 0){
+      containerText.innerHTML = `
+          <h3 class="title mb-2 title-color">Personajes</h3>
+          <p class="is-size-6 has-text-weight-bold mt-0">${arr.length} Resultado(s)</p>
+          <p class="subtitle has-text-weight-bold mt-6 title-color">No se han encontrado resultados</p>`
+  }
+  let box = '';
+  arr.forEach(character => {
+      const {name, thumbnail: {extension, path}, id} = character;
+      const pathNonFoundNowanted = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+      const pathNonFoundWanted = "https://i.pinimg.com/564x/6d/af/a0/6dafa08555ee450d9d61061c7bc23cb5";
+     
+      containerText.innerHTML = `
+              <h3 class="title mb-2 title-color">Personajes</h3>
+              <p class="is-size-6 has-text-weight-bold mt-0">${arr.length} Resultado(s)</p>`
+      box += `<div class="column is-one-fifth" onclick="getCharacterId(${id})">
+                  <div class="card-character" data-title="Character" >
+                      <img src="${path === pathNonFoundNowanted ? pathNonFoundWanted : path}.${extension}" alt="${name}" class="img-comic-character">
+                      <span class="red"></span>
+                      <p class="name is-size-5 has-text-weight-bold has-text-centered mt-1 p-3">${name}</p>
+                  </div>
+              </div> `
+  });
+container.innerHTML = box
+};
+
 
 
 
